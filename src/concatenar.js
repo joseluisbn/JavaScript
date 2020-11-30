3df894001be5855691ede91da90c444f83e174f5
José Luis Blanco <joseluisbn@gmail.com>
2020-11-30 19:33:31 +0100

    Update concatenar.js

--- src/concatenar.js
+++ src/concatenar.js
@@ -1,8 +1,8 @@
 //Concatenamos números en js como si se tratara de strings utilizando comillas al comienzo de la concatenación
 
-num1 = 4
-num2 = 3
+num1 = 4;
+num2 = 3;
 
-result = "" + num1 + num2
+result = "" + num1 + num2;
 
-document.write(result)
+document.write(result);
